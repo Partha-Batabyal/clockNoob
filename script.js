e1d66930c3b684aa40a8a5080c6a9b87c7e237c7
Partha Batabyal <163519640+Partha-Batabyal@users.noreply.github.com>
2024-03-27 22:23:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 let p = document.querySelector(".p");
 let m;
 
-
-
 setInterval(() => {
   let date = new Date();
   let hours = date.getHours().toString().padStart(2, "0");
   let minutes = date.getMinutes().toString().padStart(2, "0");
   let seconds = date.getSeconds().toString().padStart(2, "0");
   m = `${hours}:${minutes}:${seconds}`;
-    p.innerHTML = m;
+  p.innerHTML = m;
 
-   let speechMessage;
-   if (hours >= 1 && hours < 12) {
-     speechMessage = "Good Morning, sir";
-   } else if (hours >= 12 && hours < 18) {
-     speechMessage = "Good Afternoon, sir";
-   } else if (hours >= 18 && hours < 24) {
-     speechMessage = "Good Evening, sir";
-   } else {
-     speechMessage = "Hello, sir";
-   }
-   let speech = new SpeechSynthesisUtterance(speechMessage);
-   window.speechSynthesis.speak(speech);
+    let speechMessage;
+    if (hours >= 1 && hours < 12) {
+      speechMessage = "Good Morning, sir";
+    } else if (hours >= 12 && hours < 18) {
+      speechMessage = "Good Afternoon, sir";
+    } else if (hours >= 18 && hours < 24) {
+      speechMessage = "Good Evening, sir";
+    } else {
+      speechMessage = "Hello, sir";
+    }
+
+    let speech = new SpeechSynthesisUtterance(speechMessage);
+    speech.lang = "en-US";
+    window.speechSynthesis.speak(speech);
+   
+
 }, 1000);

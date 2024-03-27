dbb16acc8b364c202bcd8dcd7633cb675d8b1e68
Partha Batabyal <163519640+Partha-Batabyal@users.noreply.github.com>
2024-03-27 22:09:44 +0530

    v2
    --version2

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
 let p = document.querySelector(".p");
 let m;
 
-window.onload = function () {
-  let date = new Date();
-  let hours = date.getHours();
-  let speechMessage;
-  if (hours >= 1 && hours < 12) {
-    speechMessage = "Good Morning, sir";
-  } else if (hours >= 12 && hours < 18) {
-    speechMessage = "Good Afternoon, sir";
-  } else if (hours >= 18 && hours < 24) {
-    speechMessage = "Good Evening, sir";
-  } else {
-    speechMessage = "Hello, sir";
-  }
-  let speech = new SpeechSynthesisUtterance(speechMessage);
-  window.speechSynthesis.speak(speech);
-};
+
 
 setInterval(() => {
   let date = new Date();
@@ -24,5 +9,18 @@ setInterval(() => {
   let minutes = date.getMinutes().toString().padStart(2, "0");
   let seconds = date.getSeconds().toString().padStart(2, "0");
   m = `${hours}:${minutes}:${seconds}`;
-  p.innerHTML = m;
+    p.innerHTML = m;
+
+   let speechMessage;
+   if (hours >= 1 && hours < 12) {
+     speechMessage = "Good Morning, sir";
+   } else if (hours >= 12 && hours < 18) {
+     speechMessage = "Good Afternoon, sir";
+   } else if (hours >= 18 && hours < 24) {
+     speechMessage = "Good Evening, sir";
+   } else {
+     speechMessage = "Hello, sir";
+   }
+   let speech = new SpeechSynthesisUtterance(speechMessage);
+   window.speechSynthesis.speak(speech);
 }, 1000);
